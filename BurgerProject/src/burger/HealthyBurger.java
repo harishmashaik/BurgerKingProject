26f626429220841fd1b648987b78877448234394
haris <haris@192.168.2.13>
2020-05-18 19:42:07 -0400

    Second commit

---
+++ BurgerProject/src/burger/HealthyBurger.java
@@ -0,0 +1,10 @@
+package burger;
+
+	public class HealthyBurger extends Hamburger {
+
+
+	    public HealthyBurger(int id, String name, double price, String meat) {
+	        super(id, name, price, meat, "Brown Rye Bread");
+	    }
+	}
+
---
+++ BurgerProject/src/burger/Sides.java
@@ -0,0 +1,11 @@
+package burger;
+
+
+	public class Sides extends MenuItem {
+
+
+	    public Sides(int id, String name, double price) {
+	        super(id, name, price);
+	    }
+	}
+
